2321c748ebe782f216955a196788a04f1e8a4082
erayco16 <eray.san.pb@gmail.com>
2026-01-23 10:22:16 +0300

    degisken tanımlama

--- Assets/New Folder/selam.cs
+++ Assets/New Folder/selam.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class selam : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //print("bombaaaaaa");
+        //deðiþken tanýmlama
+        /*string ad = "Eray";
+        string soyad = "SAN";
+
+        Debug.Log("Sayýn : " + ad + " " + soyad);
+        */
+        //int yas = 24;
+        //Debug.Log(yas);
+        float ondalikliBir = 1 / 3f;
+        Debug.Log("float hassasiyeti " + ondalikliBir);
+        double ondalikliiki = 1 / 3d;
+        print("double hassasiyet " + ondalikliiki);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log("merhaba unitye hosgeldin");
     }
 }
